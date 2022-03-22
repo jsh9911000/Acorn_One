8e241d733710ea3047c3b810ed0aa36105baabcf
June <yjp243@naver.com>
2022-03-22 11:21:35 +0900

    uploadform 완성

--- src/main/java/com/one/project/product/dto/ProductDto.java
+++ src/main/java/com/one/project/product/dto/ProductDto.java
@@ -10,10 +10,10 @@ public class ProductDto {
 	private int pro_cate;
 	private String imagePath;
 	private MultipartFile image;
-	private int pro_cate;
 	
 	
 	public ProductDto() {}
+
 
 	public ProductDto(int pro_num, String pro_name, int pro_price, int remain_stock, int pro_cate, String imagePath,
 			MultipartFile image) {
@@ -26,84 +26,75 @@ public class ProductDto {
 		this.imagePath = imagePath;
 		this.image = image;
 	}
-	
 
-	public ProductDto(int pro_num, String pro_name, int pro_price, int man_sell, int woman_sell, int remain_stock,
-			String imagePath, MultipartFile image, int pro_cate) {
-		super();
-		this.pro_num = pro_num;
-		this.pro_name = pro_name;
-		this.pro_price = pro_price;
-		this.man_sell = man_sell;
-		this.woman_sell = woman_sell;
-		this.remain_stock = remain_stock;
-		this.imagePath = imagePath;
-		this.image = image;
-		this.pro_cate = pro_cate;
-	}
 
 	public int getPro_num() {
 		return pro_num;
 	}
 
+
 	public void setPro_num(int pro_num) {
 		this.pro_num = pro_num;
 	}
+
 
 	public String getPro_name() {
 		return pro_name;
 	}
 
+
 	public void setPro_name(String pro_name) {
 		this.pro_name = pro_name;
 	}
+
 
 	public int getPro_price() {
 		return pro_price;
 	}
 
+
 	public void setPro_price(int pro_price) {
 		this.pro_price = pro_price;
 	}
+
 
 	public int getRemain_stock() {
 		return remain_stock;
 	}
 
+
 	public void setRemain_stock(int remain_stock) {
 		this.remain_stock = remain_stock;
 	}
+
 
 	public int getPro_cate() {
 		return pro_cate;
 	}
 
+
 	public void setPro_cate(int pro_cate) {
 		this.pro_cate = pro_cate;
 	}
+
 
 	public String getImagePath() {
 		return imagePath;
 	}
 
+
 	public void setImagePath(String imagePath) {
 		this.imagePath = imagePath;
 	}
+
 
 	public MultipartFile getImage() {
 		return image;
 	}
 
+
 	public void setImage(MultipartFile image) {
 		this.image = image;
 	}
-
-	public int getPro_cate() {
-		return pro_cate;
-	}
-
-	public void setPro_cate(int pro_cate) {
-		this.pro_cate = pro_cate;
-	}
-
+	
 }
